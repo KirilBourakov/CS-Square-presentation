12425455936b58d9ef9114f02aa6a4fee86a7a1b
Kiril B <kirbou06012@gmail.com>
2024-11-02 22:26:29 -0300

    Finished final example

--- Site_v4/server.py
+++ Site_v4/server.py
@@ -1,23 +1,23 @@
 import sqlite3
-from flask import Flask 
-from flask import render_template 
+from flask import Flask, render_template, request
 
 # use flask --app server run --debug to run in debug mode
 
 # initalize an app
 app = Flask(__name__)
 
-def create_database():
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, title TEXT, post TEXT)''')
-    conn.commit()
-    conn.close()
+database = []
 
 # when the user goes to a the / route, give them index.html
-@app.route("/") 
+@app.route("/", methods=['GET', 'POST']) 
 def index(): 
-    create_database()
-    return render_template('index.html') 
+    if request.method == 'POST':
+        if request.form['title'] != '' and request.form['content'] != '':
+            database.append({
+                'title': request.form['title'],
+                'content': request.form['content']
+            })
+        print(database)
+    return render_template('index.html', posts=database) 
 
 
